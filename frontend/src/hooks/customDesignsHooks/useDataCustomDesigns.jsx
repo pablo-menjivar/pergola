7297e679ff6aa15b4933aa2aa0c24dbfb0b6101c
Pablo Menjívar <134906528+pablo-menjivar@users.noreply.github.com>
2025-10-23 11:01:05 -0600

    ~

--- frontend/src/hooks/customDesignsHooks/useDataCustomDesigns.jsx
+++ frontend/src/hooks/customDesignsHooks/useDataCustomDesigns.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useCallback } from "react"
 import { toast } from "react-hot-toast"
 
 // Hook para manejar datos de diseños únicos
